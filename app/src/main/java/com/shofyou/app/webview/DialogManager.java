a80785e47002fb7ce0e88d4d6a672520bcb5440e
alexpaolo244-blip <alexpaolo244@gmail.com>
2026-02-08 22:39:46 +0100

    Update DialogManager.java

--- app/src/main/java/com/shofyou/app/webview/DialogManager.java
+++ app/src/main/java/com/shofyou/app/webview/DialogManager.java
@@ -1,4 +1,4 @@
-package shofyou.app.webview;
+package com.shofyou.app.webview;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
@@ -16,14 +16,14 @@ import java.lang.ref.Reference;
 import java.lang.ref.WeakReference;
 import java.util.List;
 
-import static sngine.app.webview.IntentHelper.createIntentForAmazonAppstore;
-import static sngine.app.webview.IntentHelper.createIntentForGooglePlay;
-import static sngine.app.webview.PreferenceHelper.setAgreeShowDialog;
-import static sngine.app.webview.PreferenceHelper.setRemindInterval;
-import static sngine.app.webview.UriHelper.getAmazonAppstore;
-import static sngine.app.webview.UriHelper.getGooglePlay;
-import static sngine.app.webview.UriHelper.isPackageExists;
-import static sngine.app.webview.Utils.getDialogBuilder;
+import static com.shofyou.app.webview.IntentHelper.createIntentForAmazonAppstore;
+import static com.shofyou.app.webview.IntentHelper.createIntentForGooglePlay;
+import static com.shofyou.app.webview.PreferenceHelper.setAgreeShowDialog;
+import static com.shofyou.app.webview.PreferenceHelper.setRemindInterval;
+import static com.shofyou.app.webview.UriHelper.getAmazonAppstore;
+import static com.shofyou.app.webview.UriHelper.getGooglePlay;
+import static com.shofyou.app.webview.UriHelper.isPackageExists;
+import static com.shofyou.app.webview.Utils.getDialogBuilder;
 
 final class DialogManager {
 
